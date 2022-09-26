6f613023fdf9c9eafc4d22c2a364c970767b8759
Rii <rianne.blom@student.hu.nl>
2022-09-26 14:02:29 +0200

    App template ionic react

---
+++ 1-1-diet/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1:1Diet',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
