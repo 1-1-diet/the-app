65655a917ccad025eb0c45d82ca57a12699abdec
JesseElbertsen <91599008+JesseElbertsen@users.noreply.github.com>
2022-10-17 13:49:36 +0200

    Home page opzet

--- 1-1-diet/src/pages/Tab1.tsx
+++ 1-1-diet/src/pages/Tab1.tsx
@@ -1,22 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import kalMeter from '../Images/kalMeter.png'
+import headerbackground from '../Images/headerbackground.png'
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+    <IonPage >
+      <IonHeader >
+        <IonToolbar className="headerbackground">
+          <IonTitle className="headerbackground">Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+        <IonGrid>
+          <IonRow className="center">
+            <IonCol>
+              <div>Resultaten van deze week</div>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <div>
+                <p>Wat je bent verloren:</p>
+                <h1>-4kg</h1>
+                <p>Jij bent bij stap:</p>
+                <h1>2</h1>
+              </div>
+            </IonCol>
+            <IonCol className="center">
+              <div>
+                <img src={kalMeter} alt="kalMeter" />
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
