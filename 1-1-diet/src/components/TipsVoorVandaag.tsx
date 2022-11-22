02ffa5d9f94964a62e7093adb5dad2d18ff70bbc
Luuk van Dijk <Luukvandijk2003@gmail.com>
2022-11-22 15:23:32 +0100

    filter button verbeterd

--- 1-1-diet/src/components/TipsVoorVandaag.tsx
+++ 1-1-diet/src/components/TipsVoorVandaag.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { IonButton, IonLabel } from "@ionic/react";
 import "swiper/css";
 import styles from "./TipsVoorVandaag.module.css";
@@ -6,9 +7,7 @@ import { Recept } from "./recept";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function TipsVoorVandaag() {
-  function setLeft(left: any) {
-    document.documentElement.style.setProperty("--position-change", left);
-  }
+  const [page, setPage] = useState(1);
 
   interface Recept {
     title: string;
@@ -16,26 +15,44 @@ export default function TipsVoorVandaag() {
     yummies: string;
   }
 
+  // title={
+  //   element.title === undefined
+  //     ? "Geen title gekozen"
+  //     : element.title
+  // }
+
   return (
     <div className={styles.mainContent}>
       <div className={styles.centerHeader}>
         <div className={styles.headerContent}>
           <div className={styles.titleContent}>
-            <IonLabel id={styles.title}>wajow</IonLabel>
+            <IonLabel id={styles.title}>Tips van de week</IonLabel>
             <IonLabel id={styles.toonAlle}>Toon alle</IonLabel>
           </div>
           <div className={styles.btnContent}>
-            <div className={styles.btnSelector}></div>
-            <IonButton fill="clear" onClick={() => setLeft("1.3em")}>
+            {/* <div className={styles.btnSelector}></div> */}
+            <IonButton
+              color={page === 1 ? "navigation" : "background"}
+              onClick={() => setPage(1)}
+            >
               Ontbijt
             </IonButton>
-            <IonButton fill="clear" onClick={() => setLeft("7.3em")}>
+            <IonButton
+              color={page === 2 ? "navigation" : "background"}
+              onClick={() => setPage(2)}
+            >
               Lunch
             </IonButton>
-            <IonButton fill="clear" onClick={() => setLeft("13.2em")}>
+            <IonButton
+              color={page === 3 ? "navigation" : "background"}
+              onClick={() => setPage(3)}
+            >
               Diner
             </IonButton>
-            <IonButton fill="clear" onClick={() => setLeft("19.1em")}>
+            <IonButton
+              color={page === 4 ? "navigation" : "background"}
+              onClick={() => setPage(4)}
+            >
               Snack
             </IonButton>
           </div>
