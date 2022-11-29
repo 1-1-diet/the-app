94c65c17d1c97509d7802318c8db8b159847004c
Akhie1999 <94643478+Akhie1999@users.noreply.github.com>
2022-11-29 03:18:58 +0100

    Modal-bijna klaar

--- 1-1-diet/src/components/Invite-friend-card/Invite-friend-card.tsx
+++ 1-1-diet/src/components/Invite-friend-card/Invite-friend-card.tsx
@@ -1,38 +1,112 @@
-import React from "react";
-import { IonPage, IonCard, IonButton, IonText, IonContent } from "@ionic/react";
+import React, { useState, useRef, useEffect } from "react";
 import {
-  Autoplay,
-  Keyboard,
-  Pagination,
-  Scrollbar,
-  Zoom,
-  Virtual,
-} from "swiper";
+  IonButtons,
+  IonButton,
+  IonModal,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAvatar,
+  IonImg,
+} from "@ionic/react";
+import { IonCard, IonText } from "@ionic/react";
 import "./Invite-friend-card.css";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+function Invite() {
+  const modal = useRef<HTMLIonModalElement>(null);
+  const page = useRef(null);
 
-const Home: React.FC = () => {
+  const [presentingElement, setPresentingElement] =
+    useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setPresentingElement(page.current);
+  }, []);
+
+  function dismiss() {
+    modal.current?.dismiss();
+  }
+
   return (
-    <IonContent>
-      <IonCard className="ivite-card">
-        <div className="textbox">
-          <IonText>
-            <h5 className="ivite-card-text">
-              Nodig je vriend via de uit en krijg 20 euro kortting op je
-              volgende besteling
-            </h5>
-          </IonText>
-          <IonButton color="primary">
-            <h5>Lees meer</h5>
-          </IonButton>
-        </div>
+    <IonPage ref={page}>
+      <IonContent className="ion-padding">
+        <IonCard className="ivite-card">
+          <div className="textbox">
+            <IonText>
+              <h5 className="ivite-card-text">
+                Nodig je vriend via de uit en krijg 20 euro kortting op je
+                volgende besteling
+              </h5>
+            </IonText>
 
-        <div className="ivite-card-logo-coins">
-          <div className="ivite-card-logo" />
-        </div>
-      </IonCard>
-    </IonContent>
+            <IonButton id="open-modal" expand="block">
+              Lees Meer
+            </IonButton>
+          </div>
+
+          <div className="ivite-card-logo-coins">
+            <div className="ivite-card-logo" />
+          </div>
+        </IonCard>
+
+        <IonModal
+          ref={modal}
+          trigger="open-modal"
+          presentingElement={presentingElement!}
+        >
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Nodig je vriend nu uit </IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => dismiss()}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <IonList>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=b" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Connor Smith</h2>
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=a" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Daniel Smith</h2>
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=d" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Greg Smith</h2>
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://i.pravatar.cc/300?u=e" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Zoey Smith</h2>
+                </IonLabel>
+              </IonItem>
+            </IonList>
+          </IonContent>
+        </IonModal>
+      </IonContent>
+    </IonPage>
   );
-};
-export default Home;
+}
+
+export default Invite;
