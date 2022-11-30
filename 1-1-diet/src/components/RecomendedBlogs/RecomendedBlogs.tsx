f8118bd7e2a5fcb569fd1955cb08369b0c2dbb50
Rii <rianne.blom@student.hu.nl>
2022-11-30 15:43:08 +0100

    Blogs af inclusief styling
    yaaaaaay

--- 1-1-diet/src/components/RecomendedBlogs/RecomendedBlogs.tsx
+++ 1-1-diet/src/components/RecomendedBlogs/RecomendedBlogs.tsx
@@ -1,34 +1,20 @@
 import React, { Component } from "react";
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonButton,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonLabel,
-} from "@ionic/react";
+import { IonCard, IonIcon, IonLabel } from "@ionic/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "./RecomendedBlogs.css";
+import { book } from "ionicons/icons";
 
 import { Blogs } from "./Blogs";
 
 const RecomendedBlogs: React.FC = () => {
   return (
     <div>
-      <div>
-        <h1>Hello world?</h1>
+      <div className="titleContent">
+        <h1 id="Title">Aanbevolen blogs</h1>
+        <IonLabel id="toonAlle">Toon Alle</IonLabel>
       </div>
-      <Swiper slidesPerView={1.3} grabCursor={true}>
+      <Swiper className="swiper" slidesPerView={1.2} grabCursor={true}>
         {Blogs?.map((element: any) => {
           return (
             <SwiperSlide key={element.title}>
@@ -38,9 +24,10 @@ const RecomendedBlogs: React.FC = () => {
                   src={require(`../../Images/${element.img}`)}
                 />
                 <div className="BlogText">
-                  <p>{element.title} </p>
+                  <IonLabel className="titleContent">{element.title} </IonLabel>
                   <div className="Ptext">
                     <p>{element.readTime}</p>
+                    <IonIcon icon={book} />
                     <p>{element.date}</p>
                   </div>
                 </div>
