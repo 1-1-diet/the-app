3c1df9e7e879252c7dbae203e4d5c129a6dfa514
Diego Rolf <djdiegorof@gmail.com>
2022-12-12 10:24:08 +0100

    fixed naming
    Fixed the naming and style of the page

--- 1-1-diet/src/components/Invite-friend-card/Invite-friend-card.tsx
+++ 1-1-diet/src/components/Invite-friend-card/Invite-friend-card.tsx
@@ -56,8 +56,8 @@ function Invite() {
     });
   }
 
-  const frikandel: any = () => {
-    console.log("deze werkt");
+  function sendToWhatsapp() {
+    window.location.href ="https://wa.me/?text=Als%20jij%20via%20mijn%20link%20je%20aanmeld%20krijgen%20we%20alle%202%20een%20leuke%20bonus%0a1op1dieet.nl"
   };
 
   async function copyText() {
@@ -67,21 +67,18 @@ function Invite() {
   const modalLinks = [
     {
       title: 1,
-      href: "https://wa.me/?text=Als%20jij%20via%20mijn%20link%20je%20aanmeld%20krijgen%20we%20alle%202%20een%20leuke%20bonus%0a1op1dieet.nl",
-      onClick: frikandel,
+      onClick: sendToWhatsapp,
       img: "https://w7.pngwing.com/pngs/922/489/png-transparent-whatsapp-icon-logo-whatsapp-logo-whatsapp-logo-text-trademark-grass-thumbnail.png",
       label: "Stuur link via WhatsApp",
     },
     {
       title: 2,
-      href: "#!",
       onClick: createAlert,
       img: "https://helpdeskgeek.com/wp-content/pictures/2021/03/scan-qr-code.jpg",
       label: "Maak een QR code",
     },
     {
       title: 3,
-      href: "#!",
       onClick: copyText,
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUA7MG3vYHRFDOnDzsUUXBP3tKG2wbDBHLDh-UliAo2zr50fx_spbaKWKcpSz88dhn5q0&usqp=CAU",
       label: "Kopie link",
@@ -130,11 +127,11 @@ function Invite() {
                   <IonItem
                     button
                     key={links.title}
-                    href={links.href}
                     onClick={links.onClick}
                   >
-                    <IonAvatar slot="start"></IonAvatar>
+                    <IonAvatar slot="start">
                     <IonImg src={links.img} />
+                    </IonAvatar>
                     <IonLabel>
                       <h2>{links.label}</h2>
                     </IonLabel>
@@ -142,30 +139,6 @@ function Invite() {
                 );
               })}
             </div>
-            {/* <IonItem href="https://wa.me/?text=Als%20jij%20via%20mijn%20link%20je%20aanmeld%20krijgen%20we%20alle%202%20een%20leuke%20bonus%0a1op1dieet.nl">
-              <IonAvatar slot="start">
-                <IonImg src="https://w7.pngwing.com/pngs/922/489/png-transparent-whatsapp-icon-logo-whatsapp-logo-whatsapp-logo-text-trademark-grass-thumbnail.png" />
-              </IonAvatar>
-              <IonLabel>
-                <h2>Stuur link via WhatsApp</h2>
-              </IonLabel>
-            </IonItem>
-            <IonItem button>
-              <IonAvatar slot="start">
-                <IonImg src="https://helpdeskgeek.com/wp-content/pictures/2021/03/scan-qr-code.jpg" />
-              </IonAvatar>
-              <IonLabel>
-                <h2>Maak een QR code</h2>
-              </IonLabel>
-            </IonItem>
-            <IonItem button>
-              <IonAvatar slot="start">
-                <IonImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUA7MG3vYHRFDOnDzsUUXBP3tKG2wbDBHLDh-UliAo2zr50fx_spbaKWKcpSz88dhn5q0&usqp=CAU" />
-              </IonAvatar>
-              <IonLabel>
-                <h2>Kopie link </h2>
-              </IonLabel>
-            </IonItem> */}
           </IonList>
         </IonContent>
       </IonModal>
