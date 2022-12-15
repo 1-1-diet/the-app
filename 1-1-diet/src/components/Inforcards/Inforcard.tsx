ca28ba89488d3bfd0582fc6cd64684570bd78892
Akhie1999 <94643478+Akhie1999@users.noreply.github.com>
2022-12-15 12:47:01 +0100

    .map is done

--- 1-1-diet/src/components/Inforcards/Inforcard.tsx
+++ 1-1-diet/src/components/Inforcards/Inforcard.tsx
@@ -18,16 +18,17 @@ import {
 } from "@ionic/react";
 import styles from "./Infocard.module.css";
 import {} from "@ionic/react";
+import { info } from "console";
+import { Vragen } from "./vragen";
 
-function Info() {
+export const Info = (vragen: any) => {
   const modal = useRef<HTMLIonModalElement>(null);
-
   function dismiss() {
     modal.current?.dismiss();
   }
   return (
     <div className={styles.cardone}>
-      <div className={styles.logoone}></div>
+      <div className={styles.logoone} />
       <div className={styles.textbox}>
         <IonText>
           <h5 className={styles.cardonetext}>
@@ -49,66 +50,27 @@ function Info() {
               </IonButtons>
             </IonToolbar>
           </IonHeader>
-
           <IonImg src="https://www.one2onediet-westafrica.com/getmedia/Yoga-Lady-and-Consultant-on-sofa-320x320.png" />
           <IonAccordionGroup>
-            <IonAccordion value="first">
-              <IonItem slot="header" color="light">
-                <IonLabel>
-                  <h2> Het is toch geen crashdieet hè?</h2>
-                </IonLabel>
-              </IonItem>
-              <div className="ion-padding" slot="content">
-                <p>
-                  Nee, alle maaltijdvervangers zijn medisch wetenschappelijk
-                  samengesteld. Je loopt tijdens dit dieet geen tekorten op. De
-                  vervangers zijn puur ondersteunend en alleen tijdelijk bedoeld
-                  om jou het zetje in de goede richting te geven. Als je in stap
-                  1 of stap 2 start, kun je de eerste paar dagen een licht
-                  hongerig gevoel ervaren. Zodra je vetverbranding op gang komt,
-                  zal dit afnemen
-                </p>
-              </div>
-            </IonAccordion>
-            <IonAccordion value="second">
-              <IonItem slot="header" color="light">
-                <IonLabel>
-                  <h2>Zijn er bijwerkingen?</h2>
-                </IonLabel>
-              </IonItem>
-              <div className="ion-padding" id="antwoord" slot="content">
-                <p>
-                  Bij elk dieet kunnen bijwerkingen optreden. De consulent neemt
-                  bij het intakegesprek jouw medisch dossier door en kijkt welke
-                  stap uit Het Stappenplan het beste bij jou past. Bij twijfel
-                  vindt overleg met de huisarts of specialist plaats. Ook jouw
-                  persoonlijke geschiedenis en familiegeschiedenis worden hierin
-                  betrokken.
-                </p>
-              </div>
-            </IonAccordion>
-            <IonAccordion value="third">
-              <IonItem slot="header" color="light">
-                <IonLabel>
-                  <h2>
-                    Hebben jullie ook glutenvrije en lactosevrije producten?
-                  </h2>
-                </IonLabel>
-              </IonItem>
-              <div className="ion-padding" slot="content">
-                <p>
-                  Ja, afhankelijk van de aard van de intolerantie en/of allergie
-                  kun je het dieet ook volgen. Het aantal producten en smaken
-                  zijn hierin wel beperkter. Voor meer informatie kijk je onder
-                  'Producten'.
-                </p>
-              </div>
-            </IonAccordion>
+            {Vragen?.map((vragen: any) => {
+              return (
+                <IonAccordion value={vragen.title} key={vragen.title}>
+                  <IonItem slot="header" color="light">
+                    <IonLabel>
+                      <h2>{vragen.title}</h2>
+                    </IonLabel>
+                  </IonItem>
+                  <div className="ion-padding" slot="content">
+                    <p>{vragen.antwoord}</p>
+                  </div>
+                </IonAccordion>
+              );
+            })}
           </IonAccordionGroup>
         </IonContent>
       </IonModal>
     </div>
   );
-}
+};
 
 export default Info;
---
+++ 1-1-diet/src/components/Inforcards/vragen.js
@@ -0,0 +1,17 @@
+export const Vragen = [
+  {
+    title: "Het is toch geen crashdieet hè?",
+    antwoord:
+      "Nee, alle maaltijdvervangers zijn medisch wetenschappelijk samengesteld. Je loopt tijdens dit dieet geen tekorten op. Devervangers zijn puur ondersteunend en alleen tijdelijk bedoeld om jou het zetje in de goede richting te geven. Als je in stap 1 of stap 2 start, kun je de eerste paar dagen een licht hongerig gevoel ervaren. Zodra je vetverbranding op gang komt,zal dit afnemen",
+  },
+  {
+    title: "Hebben jullie ook glutenvrije en lactosevrije producten?",
+    antwoord:
+      "Ja, afhankelijk van de aard van de intolerantie en/of allergie kun je het dieet ook volgen. Het aantal producten en smaken zijn hierin wel beperkter. Voor meer informatie kijk je onder producten.",
+  },
+  {
+    title: "Zijn er bijwerkingen?",
+    antwoord:
+      "Bij elk dieet kunnen bijwerkingen optreden. De consulent neemt bij het intakegesprek jouw medisch dossier door en kijkt welke stap uit Het Stappenplan het beste bij jou past. Bij twijfel vindt overleg met de huisarts of specialist plaats. Ook jouw persoonlijke geschiedenis en familiegeschiedenis worden hierin betrokken.",
+  },
+];
