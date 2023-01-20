b6b3dbef1e9f378289a2c3c7b7e658b15cb39c25
JesseElbertsen <91599008+JesseElbertsen@users.noreply.github.com>
2023-01-20 12:34:22 +0100

    update

--- 1-1-diet/src/components/SearchPage/SearchPage.tsx
+++ 1-1-diet/src/components/SearchPage/SearchPage.tsx
@@ -6,23 +6,35 @@ import {
   IonCol,
   IonSearchbar,
   IonIcon,
+  IonButton,
 } from "@ionic/react";
 import style from "./SearchPage.module.css";
-import { optionsOutline } from "ionicons/icons";
+import { optionsOutline, cartOutline } from "ionicons/icons";
 
 export default function SearchPage() {
   return (
     <div>
-      <IonText>Vind jouw favoriete maaltijd!</IonText>
+      <IonGrid>
+        <IonRow>
+          <IonCol size="10">
+            <IonText>
+              <h1>Vind jouw favoriete maaltijd!</h1>
+            </IonText>
+          </IonCol>
+          <IonCol className="ion-margin-top">
+            <IonIcon icon={cartOutline} size="large" color="primary"></IonIcon>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
       <IonGrid>
         <IonRow>
           <IonCol size="10">
             <IonSearchbar></IonSearchbar>
           </IonCol>
           <IonCol size="2">
-            <button>
-              <IonIcon icon={optionsOutline} />
-            </button>
+            <IonButton fill="clear">
+              <IonIcon icon={optionsOutline} size="medium" color="secondary" />
+            </IonButton>
           </IonCol>
         </IonRow>
       </IonGrid>
