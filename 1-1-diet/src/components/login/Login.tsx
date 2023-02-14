c1eba5146c92dda741772b3dbe71c8c8200c3594
Luuk van Dijk <Luukvandijk2003@gmail.com>
2023-02-14 17:31:34 +0100

    begonnen met de login page

--- 1-1-diet/src/components/login/Login.tsx
+++ 1-1-diet/src/components/login/Login.tsx
@@ -1,4 +1,4 @@
-import { IonLabel, IonIcon } from "@ionic/react";
+import { IonCheckbox, IonText, IonInput, IonButton } from "@ionic/react";
 import styles from "./Login.module.css";
 import BgPtrn from "../Splashscreen/BgPtrn";
 import klap from "../../Images/1to1girls.svg";
@@ -9,6 +9,20 @@ export default function SplashScreen() {
       <BgPtrn />
       <div className={styles.mainContent}>
         <img id={styles.klap} src={klap} alt="two girl giving a high 5" />
+        <IonText id={styles.title}>Welkom terug</IonText>
+        <div className={styles.inputFields}>
+          <IonInput className={styles.input} placeholder="Email" />
+          <IonInput className={styles.input} placeholder="Wachtwoord" />
+        </div>
+        <div className={styles.extraContent}>
+          <div className={styles.stayLogged}>
+            <IonCheckbox />
+            <IonText>Ingelogd blijven</IonText>
+          </div>
+          <IonText>Wachtwoord vergeten?</IonText>
+        </div>
+        <IonButton>Log in</IonButton>
+        <IonText>Nog geen client? Maak nu een account</IonText>
       </div>
     </div>
   );
