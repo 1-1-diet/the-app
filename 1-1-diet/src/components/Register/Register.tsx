0b5429251603ce56b530a733f9463761eaed08e3
JesseElbertsen <91599008+JesseElbertsen@users.noreply.github.com>
2023-02-17 11:04:12 +0100

    final register

--- 1-1-diet/src/components/Register/Register.tsx
+++ 1-1-diet/src/components/Register/Register.tsx
@@ -6,47 +6,70 @@ import {
   IonContent,
   IonItem,
   IonLabel,
+  IonCol,
+  IonIcon,
 } from "@ionic/react";
+import {
+  personOutline,
+  callOutline,
+  mailOutline,
+  lockClosedOutline,
+} from "ionicons/icons";
 import styles from "./Register.module.css";
 import BgPtrn from "../Splashscreen/BgPtrn";
 import klap from "../../Images/logo2.svg";
 
 export default function Register() {
+  const register = [
+    {
+      title: " Voor en Achternaam",
+      icon: personOutline,
+    },
+    {
+      title: " Mobiel nummer",
+      icon: callOutline,
+    },
+    {
+      title: " Email",
+      icon: mailOutline,
+    },
+    {
+      title: " wachtwoord",
+      icon: lockClosedOutline,
+    },
+  ];
+
   return (
     <div className={styles.mainContainer}>
       <BgPtrn />
       <div className={styles.mainContent}>
         <img id={styles.klap} src={klap} alt="logo" />
-
         <div>
-          <div>
-            <IonItem>
-              <IonLabel position="floating">Voor en Achternaam</IonLabel>
-              <IonInput placeholder="Enter text"></IonInput>
-            </IonItem>
-            <IonItem>
-              <IonLabel position="floating">Mobiel nummer</IonLabel>
-              <IonInput placeholder="Enter text"></IonInput>
-            </IonItem>
-            <IonItem>
-              <IonLabel position="floating">Email</IonLabel>
-              <IonInput placeholder="Enter text"></IonInput>
-            </IonItem>
-            <IonItem>
-              <IonLabel position="floating">Wachtwoord</IonLabel>
-              <IonInput placeholder="Enter text"></IonInput>
-            </IonItem>
-          </div>
+          <IonCol>
+            {register?.map((element: any) => {
+              return (
+                <IonCol size="6" key={element.title}>
+                  <div className={styles.inputContainer}>
+                    <IonItem lines="none">
+                      <IonIcon icon={element.icon} color="secondary" />
+                      <IonInput placeholder={element.title} />
+                    </IonItem>
+                  </div>
+                </IonCol>
+              );
+            })}
+          </IonCol>
         </div>
         <div className={styles.extraContent}>
           <div className={styles.stayLogged}>
             <IonCheckbox />
             <IonText>Ingelogd blijven</IonText>
-            <IonText>Heb je al een account?</IonText>
-            <a>Log in</a>
+            <IonText>
+              Heb je al een account?<IonText> Log in</IonText>
+            </IonText>
           </div>
         </div>
-        <div>
+        <div className={styles.button}>
           <IonButton color="secondary">Volgende</IonButton>
         </div>
       </div>
