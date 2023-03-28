3fa1f57d0ba825c05f0234da665c1a172ff73ae3
Akhie1999 <94643478+Akhie1999@users.noreply.github.com>
2023-03-28 06:10:16 +0200

    GegevensKlaar

--- 1-1-diet/src/pages/Tab3/Tab3.tsx
+++ 1-1-diet/src/pages/Tab3/Tab3.tsx
@@ -1,18 +1,15 @@
 import { IonContent, IonPage } from "@ionic/react";
 import "./Tab3.css";
 import Header from "../../components/profiel/header";
-import QuickOptions from "../../components/profiel/quichOptions";
-import Card from "../../components/profiel/card";
+import Gegevens from "../../components/Gegevens/Gegevens";
+import Betaling from "../../components/Betaalmethode/Betaalmethode";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-        <Header name="Hendrik Jansen" />
-        <div className="mainContent">
-          <QuickOptions />
-          <Card />
-        </div>
+        <Gegevens />
+        <Betaling />
       </IonContent>
     </IonPage>
   );
