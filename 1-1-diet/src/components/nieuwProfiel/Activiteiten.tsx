acd69d3fb7ab0d1cab0fd2c66c16849e320631e2
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-03-28 12:58:00 +0200

    activiteiten

---
+++ 1-1-diet/src/components/nieuwProfiel/Activiteiten.tsx
@@ -0,0 +1,123 @@
+import { IonButton, IonText } from "@ionic/react";
+import React from "react";
+import styles from "./Activiteiten.module.css";
+
+export default function Activiteiten() {
+  const swiperCards = [
+    {
+      title: "Weeg moment",
+      icon: "scale.svg",
+      color: "#f57e71",
+    },
+    {
+      title: "Bestellingen",
+      icon: "card.svg",
+      color: "#ffb167",
+    },
+    {
+      title: "Favorieten",
+      icon: "fav.svg",
+      color: "#679e97",
+    },
+  ];
+
+  const consulenten = [
+    {
+      img: "1to1girls.svg",
+      name: "Stella Kane",
+      location: "Utrecht",
+    },
+  ];
+
+  const afspraken = [
+    {
+      dateNum: "12",
+      dateMonth: "Jan",
+      day: "Maandag",
+      time: "11:00 uur tot 12:00 uur",
+      btn: "Samenvatting",
+      color: "#EDF1FA",
+      color2: "#D8E3FF",
+    },
+    {
+      dateNum: "13",
+      dateMonth: "Jan",
+      day: "Dinsdag",
+      time: "10:00 uur tot 11:30 uur",
+      btn: "Samenvatting",
+      color: "#F9E4EE",
+      color2: "#F4CEE0",
+    },
+  ];
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.swiperCards}>
+        {swiperCards?.map((element) => {
+          return (
+            <div key={element.title} className={styles.topCard}>
+              <img
+                style={{ backgroundColor: `${element.color}` }}
+                id={styles.topCardIcon}
+                alt="icon"
+                src={require(`./icons/${element.icon}`)}
+              />
+              <h1 id={styles.topTitle}>{element.title}</h1>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.consulentenContainer}>
+        <h1 id={styles.consulentTitle}>Mijn consulent</h1>
+        <div className={styles.center}>
+          {consulenten?.map((element) => {
+            return (
+              <div key={element.name} className={styles.consulentCardContainer}>
+                <img
+                  src={require(`../../Images/${element.img}`)}
+                  alt="profile picture"
+                  id={styles.consulentCardProfile}
+                />
+                <div className={styles.consulentDescriptionContainer}>
+                  <IonText id={styles.consulentName}>{element.name}</IonText>
+                  <IonText>{element.location}</IonText>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.afsprakenContainer}>
+        <h1 id={styles.consulentTitle}>Afspraken</h1>
+        <div className={styles.afsprakenCenter}>
+          {afspraken?.map((element) => {
+            return (
+              <div
+                key={element.dateNum}
+                className={styles.afsprakenCardContainer}
+                style={{ backgroundColor: `${element.color}` }}
+              >
+                <div
+                  className={styles.afsprakenCardIcon}
+                  style={{ backgroundColor: `${element.color2}` }}
+                >
+                  <IonText id={styles.afsprakenDateNum}>
+                    {element.dateNum}
+                  </IonText>
+                  <IonText id={styles.afsprakenDateMonth}>
+                    {element.dateMonth}
+                  </IonText>
+                </div>
+                <div className={styles.afsprakenDescriptionContainer}>
+                  <IonText id={styles.afsparkenDay}>{element.day}</IonText>
+                  <IonText>{element.time}</IonText>
+                </div>
+                <IonButton className={styles.btn}>{element.btn}</IonButton>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- 1-1-diet/src/pages/Tab3/Tab3.tsx
+++ 1-1-diet/src/pages/Tab3/Tab3.tsx
@@ -3,16 +3,18 @@ import "./Tab3.css";
 import Header from "../../components/profiel/header";
 import QuickOptions from "../../components/profiel/quichOptions";
 import Card from "../../components/profiel/card";
+import Activiteiten from "../../components/nieuwProfiel/Activiteiten";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-        <Header name="Hendrik Jansen" />
+        {/* <Header name="Hendrik Jansen" />
         <div className="mainContent">
           <QuickOptions />
           <Card />
-        </div>
+        </div> */}
+        <Activiteiten />
       </IonContent>
     </IonPage>
   );
