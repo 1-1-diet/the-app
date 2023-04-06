e7910ed5d5e30ff6aab4972feffdc6d103bb1dd3
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-04-06 16:46:47 +0200

    werkt error: voor de gene er naast

--- 1-1-diet/src/components/Navbar/Navbar.tsx
+++ 1-1-diet/src/components/Navbar/Navbar.tsx
@@ -11,65 +11,95 @@ import {
 
 export default function Navbar() {
   const [page, setPage] = useState(1);
-  const [home, setHome] = useState(styles.active);
+  const [home, setHome] = useState(true);
+  const [consulent, setConsulent] = useState(false);
+  const [profiel, setProfiel] = useState(false);
+  const [shop, setShop] = useState(false);
+  const [chat, setChat] = useState(false);
 
-  //   useEffect(() => {
-  //     if (page === 1) {
-  //       setHome(true);
-  //     } else {
-  //       setHome(false);
-  //     }
-
-  //     console.log(home);
-  //   }, [page]);
+  useEffect(() => {
+    if (page === 1) {
+      setHome(true);
+      setConsulent(false);
+      setProfiel(false);
+      setShop(false);
+      setChat(false);
+    }
+    if (page === 2) {
+      setHome(false);
+      setConsulent(true);
+      setProfiel(false);
+      setShop(false);
+      setChat(false);
+    }
+    if (page === 3) {
+      setHome(false);
+      setConsulent(false);
+      setProfiel(true);
+      setShop(false);
+      setChat(false);
+    }
+    if (page === 4) {
+      setHome(false);
+      setConsulent(false);
+      setProfiel(false);
+      setShop(true);
+      setChat(false);
+    }
+    if (page === 5) {
+      setHome(false);
+      setConsulent(false);
+      setProfiel(false);
+      setShop(false);
+      setChat(true);
+    }
+  }, [page]);
 
   const Navbar = [
     {
       title: "Home",
       icon: homeOutline,
-      active: "block",
-      style: { home },
+      active: home,
+      tab: 1,
     },
     {
       title: "Consulent",
       icon: locationOutline,
-      active: "none",
-      style: "",
+      active: consulent,
+      tab: 2,
     },
     {
       title: "Profiel",
       icon: personOutline,
-      active: "none",
-      style: "",
+      active: profiel,
+      tab: 3,
     },
     {
       title: "Shop",
       icon: cardOutline,
-      active: "none",
-      style: "",
+      active: shop,
+      tab: 4,
     },
     {
       title: "Chat",
       icon: chatbubbleEllipsesOutline,
-      active: "none",
-      style: "",
+      active: chat,
+      tab: 5,
     },
   ];
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.navContainer}>
-        {Navbar?.map((element) => {
+        {Navbar?.map((element, tab) => {
           return (
             <div
               key={element.title}
               className={styles.tabContainer}
-              id={element.style}
+              onClick={() => setPage(tab)}
             >
               <IonIcon icon={element.icon} id={styles.icon} size="large" />
-              <IonText style={{ display: `${element.active}` }}>
-                {element.title}
-              </IonText>
+              {`${element.active}` === "true" ? <h1>{element.title}</h1> : null}
             </div>
           );
         })}
