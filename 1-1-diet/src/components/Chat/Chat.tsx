6c931c9b05a3a0839e1ce12a09137d4452b47739
JesseElbertsen <91599008+JesseElbertsen@users.noreply.github.com>
2023-05-10 11:38:36 +0200

    chatpage start

--- 1-1-diet/src/components/Chat/Chat.tsx
+++ 1-1-diet/src/components/Chat/Chat.tsx
@@ -1,31 +1,17 @@
 import React from "react";
-import { IonGrid, IonCol, IonRow } from "@ionic/react";
+import { IonGrid, IonCol, IonRow, IonIcon } from "@ionic/react";
+import { call } from "ionicons/icons";
 import styles from "./Chat.module.css";
 import ptrn from "../../Images/BACKGROUND 2.svg";
 import profile1 from "./images/Photo Profile.svg";
-import profile2 from "./images/Photo Profile 2.svg";
-import profile3 from "./images/Photo Profile 3.svg";
-import team from "./images/Team.svg";
 
-export default function Chat() {
-  const chat = [
+export default function ChatList() {
+  const contactDetails = [
     {
       name: "Stefan de Jong",
       img: profile1,
-      time: "20:00",
+      icon: call,
       status: "online",
-    },
-    {
-      name: "Borus Rebel",
-      img: profile2,
-      time: "19:45",
-      status: "Bezig",
-    },
-    {
-      name: "Lissane Westerveld",
-      img: profile3,
-      time: "09:11",
-      status: "Offline",
     },
   ];
 
@@ -34,15 +20,11 @@ export default function Chat() {
       <img src={ptrn} alt="food ptrn" id={styles.headerptrn} />
       <div className={styles.content}>
         <IonGrid>
-          <IonCol>
-            <h1>Chat</h1>
-          </IonCol>
           <div className={styles.chat}>
-            <p>Consultants</p>
             <div>
-              {chat?.map((element: any) => {
+              {contactDetails?.map((element: any) => {
                 return (
-                  <IonGrid className={styles.contacts}>
+                  <IonGrid>
                     <IonRow className="ion-justify-content-between">
                       <IonRow>
                         <img src={element.img} alt="profile not found" />
@@ -52,7 +34,7 @@ export default function Chat() {
                         </IonCol>
                       </IonRow>
                       <div className={styles.time}>
-                        <p>{element.time}</p>
+                        <IonIcon icon={element.icon} />
                       </div>
                     </IonRow>
                   </IonGrid>
@@ -60,27 +42,6 @@ export default function Chat() {
               })}
             </div>
           </div>
-          <IonCol className={styles.team}>
-            <IonCol>
-              <p>1:1 Team Support</p>
-            </IonCol>
-            <IonCol>
-              <IonGrid className={styles.contacts}>
-                <IonRow className="ion-justify-content-between">
-                  <IonRow>
-                    <img src={team} alt="profile not found" />
-                    <IonCol className="ion-margin-horizontal">
-                      <p>1:1 Team</p>
-                      <p>Online</p>
-                    </IonCol>
-                  </IonRow>
-                  <div className={styles.time}>
-                    <p>14:20</p>
-                  </div>
-                </IonRow>
-              </IonGrid>
-            </IonCol>
-          </IonCol>
         </IonGrid>
       </div>
     </div>
--- 1-1-diet/src/pages/Tab5/Tab5.tsx
+++ 1-1-diet/src/pages/Tab5/Tab5.tsx
@@ -1,13 +1,14 @@
 import { IonContent, IonPage } from "@ionic/react";
 import styles from "./Tab5.module.css";
-import Chat from "../../components/Chat/Chat";
+import ChatList from "../../components/Chat/ChatList";
 import Call from "../../components/Chat/Call";
+import Chat from "../../components/Chat/Chat";
 const Tab4: React.FC = () => {
   return (
     <IonPage className={styles.mainContainer}>
       <IonContent fullscreen>
-        {/* <Chat /> */}
-        <Call />
+        <Chat />
+        {/* <Call /> */}
       </IonContent>
     </IonPage>
   );
