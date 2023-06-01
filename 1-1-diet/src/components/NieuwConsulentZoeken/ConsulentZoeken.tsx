ef2449ac9b61bf38119216d0d5f029ee4d4b1680
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-06-01 10:49:48 +0200

    fixed consulent zoeken

--- 1-1-diet/src/components/NieuwConsulentZoeken/ConsulentZoeken.tsx
+++ 1-1-diet/src/components/NieuwConsulentZoeken/ConsulentZoeken.tsx
@@ -87,119 +87,6 @@ export default function ConsulentZoeken() {
   ];
 
   return (
-    // <<<<<<< HEAD
-    //     <div className={styles.mainContainer}>
-    //       <img alt="map" src={map} id={styles.imgMap} />
-    //       <div className={styles.inputContainer}>
-    //         <IonInput placeholder="Jouw locatie" className={styles.input} />
-    //         <div className={styles.searchBtn}>Zoeken</div>
-    //       </div>
-    //       <IonButton
-    //         color="secondary"
-    //         shape="round"
-    //         className={styles.filterBtn}
-    //         id="click-trigger"
-    //       >
-    //         <IonIcon className={styles.icon} icon={optionsOutline} />
-    //       </IonButton>
-    //       <IonPopover
-    //         trigger="click-trigger"
-    //         side="bottom"
-    //         alignment="center"
-    //         triggerAction="click"
-    //       >
-    //         <div className={styles.filterContainer}>
-    //           <IonRadioGroup className={styles.filterContent} value="man-vrouw">
-    //             <IonLabel>Geslacht</IonLabel>
-    //             {filter[0].geslacht?.map((element) => {
-    //               return (
-    //                 <div key={element.title}>
-    //                   <div className={styles.textContainer}>
-    //                     <IonRadio value={element.value}></IonRadio>
-    //                     <IonLabel>{element.title}</IonLabel>
-    //                   </div>
-    //                 </div>
-    //               );
-    //             })}
-    //           </IonRadioGroup>
-    //           <IonRadioGroup
-    //             className={styles.filterContent}
-    //             value="onwardsCoach-onlineCoach"
-    //           >
-    //             <IonLabel>Specialisatie</IonLabel>
-    //             {filter[0].specialisatie?.map((element) => {
-    //               return (
-    //                 <div key={element.title}>
-    //                   <div className={styles.textContainer}>
-    //                     <IonRadio value={element.value}></IonRadio>
-    //                     <IonLabel>{element.title}</IonLabel>
-    //                   </div>
-    //                 </div>
-    //               );
-    //             })}
-    //           </IonRadioGroup>
-    //         </div>
-    //       </IonPopover>
-    //       {markers?.map((element) => {
-    //         return (
-    //           <div key={element.top} className={styles.mainContent}>
-    //             <IonButton
-    //               id={element.trigger}
-    //               expand="block"
-    //               className={styles.markerContainer}
-    //               style={{ top: `${element.top}`, left: `${element.left}` }}
-    //             >
-    //               <IonIcon icon={location} size="large" />
-    //             </IonButton>
-    //             <IonModal
-    //               trigger={element.trigger}
-    //               initialBreakpoint={0.35}
-    //               breakpoints={[0, 0.35, 0.7]}
-    //               handleBehavior="cycle"
-    //             >
-    //               <div className={styles.mainPopupContainer}>
-    //                 <h1 id={styles.title}>{element.name}</h1>
-    //                 <div className={styles.consulentContainer}>
-    //                   <img
-    //                     src={element.image}
-    //                     alt="consulent profiel foto"
-    //                     id={styles.profileImg}
-    //                   />
-    //                   <div className={styles.consulentInfo}>
-    //                     <IonText>{element.name}</IonText>
-    //                     <IonText id={styles.distance}>
-    //                       {element.distance} van je locatie
-    //                     </IonText>
-    //                   </div>
-    //                 </div>
-    //                 <div className={styles.bottomContainer}>
-    //                   <IonButton
-    //                     color={"white"}
-    //                     id={styles.afspraak}
-    //                     href="/tabs/afspraakMaken"
-    //                   >
-    //                     Afspraak maken
-    //                   </IonButton>
-    //                   <IonButton color={"secondary"} id={styles.moreInfo}>
-    //                     <IonIcon icon={chatboxEllipsesOutline} size="large" />
-    //                   </IonButton>
-    //                 </div>
-    //                 <div className={styles.aboutMe}>
-    //                   <h1 id={styles.secondTitle}>Over mij:</h1>
-    //                   <IonText>
-    //                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-    //                     Cupiditate earum nemo impedit iusto aliquid ipsam hic
-    //                     repudiandae incidunt facilis necessitatibus quidem, enim
-    //                     vitae, distinctio nulla nesciunt. Totam ratione itaque ad!
-    //                   </IonText>
-    //                 </div>
-    //                 <div className={styles.openTime}>
-    //                   <h1 id={styles.secondTitle}>Openingstijden:</h1>
-    //                   <IonText>Maandag tot Vrijdag: {element.time}</IonText>
-    //                 </div>
-    //               </div>
-    //             </IonModal>
-    // =======
     <IonPage>
       <IonContent fullscreen>
         <div className={styles.mainContainer}>
@@ -287,7 +174,11 @@ export default function ConsulentZoeken() {
                       </div>
                     </div>
                     <div className={styles.bottomContainer}>
-                      <IonButton color={"white"} id={styles.afspraak}>
+                      <IonButton
+                        color={"white"}
+                        id={styles.afspraak}
+                        href="/tabs/afspraakMaken"
+                      >
                         Afspraak maken
                       </IonButton>
                       <IonButton color={"secondary"} id={styles.moreInfo}>
