d7e24ec225576023442f39827956286011d16e7b
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-06-06 13:55:34 +0200

    Added gegevens error email doesnt not very

--- 1-1-diet/src/components/nieuwProfiel/gegevens/Gegevens.tsx
+++ 1-1-diet/src/components/nieuwProfiel/gegevens/Gegevens.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   IonGrid,
   IonText,
@@ -6,33 +6,232 @@ import {
   IonRow,
   IonIcon,
   IonCard,
+  IonSelect,
+  IonSelectOption,
+  IonDatetime,
+  IonButtons,
+  IonButton,
+  IonItem,
+  IonInput,
 } from "@ionic/react";
 import styles from "./Gegevens.module.css";
 import betaal from "./Betaalmethode.module.css";
 import { Settings } from "./UserSettings";
 import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  chevronForwardOutline,
+  transgenderOutline,
+  calendarOutline,
+  homeOutline,
+  mailOutline,
+  callOutline,
+} from "ionicons/icons";
 
 import { UserProfile } from "../../data";
 
 export default function Instellingen() {
+  const [showDate, setShowDate] = useState(false);
+  const toggleShowDate = () => {
+    setShowDate(!showDate);
+  };
+
+  const [date, setDate] = useState<string | string[]>("2013-03-10T02:00:00Z");
+  const datetime = useRef<null | HTMLIonDatetimeElement>(null);
+  const reset = () => {
+    datetime.current?.reset();
+  };
+  const cancel = () => {
+    datetime.current?.cancel();
+    toggleShowDate();
+  };
+  const confirm = () => {
+    datetime.current?.confirm();
+    toggleShowDate();
+  };
+
+  const [adress, setAdress] = useState("siliciumweg 12a");
+  const [showAdress, setShowAdress] = useState(false);
+  const toggleShowAdress = () => {
+    setShowAdress(!showDate);
+  };
+
+  const [email, setEmail] = useState("1op1@dieet.nl");
+  const [showEmail, setShowEmail] = useState(false);
+  const toggleShowEmail = () => {
+    setShowEmail(!showEmail);
+  };
+
+  const [nummer, setNummer] = useState("06 12345678");
+  const [showNummer, setShowNummer] = useState(false);
+  const toggleShowNummer = () => {
+    setShowNummer(!showNummer);
+  };
   return (
     <div>
       <IonGrid>
         <IonCol className={styles.center}>
-          {Settings?.map((element: any) => {
-            return (
-              <div className={styles.row} key={element.title}>
-                <IonRow>
-                  <IonIcon icon={element.icon} size="large" color="secondary" />
-                  <IonText className={styles.info}>{element.title}</IonText>
-                </IonRow>
-                <IonRow>
-                  <IonText className={styles.data}>{element.gegevens}</IonText>
-                  <IonIcon icon={element.icon2} size="large" />
-                </IonRow>
-              </div>
-            );
-          })}
+          {/* Geslacht */}
+          <div className={styles.container}>
+            <div className={styles.titleContainer}>
+              <IonIcon
+                icon={transgenderOutline}
+                size="large"
+                color="secondary"
+              />
+              <IonText>Geslacht</IonText>
+            </div>
+            <IonSelect
+              itemType="Alert"
+              interfaceOptions={{ header: "Kies jouw geslacht" }}
+              interface="alert"
+              placeholder="Man"
+            >
+              <IonSelectOption value="bacon">Man</IonSelectOption>
+              <IonSelectOption value="onions">Vrouw</IonSelectOption>
+              <IonSelectOption value="pepperoni">Anders</IonSelectOption>
+            </IonSelect>
+          </div>
+          {/* Geboortedatum */}
+          <div className={styles.container}>
+            <div className={styles.titleContainer}>
+              <IonIcon icon={calendarOutline} size="large" color="secondary" />
+              <IonText>Geboortedatum</IonText>
+            </div>
+            <div onClick={toggleShowDate} className={styles.titleContainer}>
+              <IonText>{date.slice(0, 10)}</IonText>
+              <IonIcon
+                icon={chevronForwardOutline}
+                size="large"
+                color="secondary"
+              />
+            </div>
+            <div
+              id={showDate ? `` : `${styles.hide}`}
+              className={styles.backgroundBlur}
+            >
+              <IonDatetime
+                ref={datetime}
+                presentation="date"
+                value={date}
+                onIonChange={(e) => {
+                  if (!e.detail?.value) {
+                    return;
+                  }
+                  setDate(e.detail.value);
+                }}
+              >
+                <IonButtons slot="buttons">
+                  <IonButton color="danger" onClick={reset}>
+                    Reset
+                  </IonButton>
+                  <IonButton color="primary" onClick={cancel}>
+                    Cancel
+                  </IonButton>
+                  <IonButton color="primary" onClick={confirm}>
+                    Done
+                  </IonButton>
+                </IonButtons>
+              </IonDatetime>
+            </div>
+          </div>
+          {/* Adress */}
+          <div className={styles.container}>
+            <div className={styles.titleContainer}>
+              <IonIcon icon={homeOutline} size="large" color="secondary" />
+              <IonText>Adress</IonText>
+            </div>
+            <div onClick={toggleShowAdress} className={styles.titleContainer}>
+              <IonText>{adress.substring(0, 10)}...</IonText>
+              <IonIcon
+                icon={chevronForwardOutline}
+                size="large"
+                color="secondary"
+              />
+            </div>
+            <div
+              id={showAdress ? `` : `${styles.hide}`}
+              className={styles.backgroundBlur}
+            >
+              <IonItem>
+                <IonInput
+                  placeholder="Adress"
+                  onIonChange={(e) => {
+                    if (!e.detail?.value) {
+                      return;
+                    }
+                    setAdress(e.detail.value);
+                  }}
+                />
+                <IonButton onClick={() => setShowAdress(false)}>Done</IonButton>
+              </IonItem>
+            </div>
+          </div>
+          {/* Email */}
+          <div className={styles.container}>
+            <div className={styles.titleContainer}>
+              <IonIcon icon={mailOutline} size="large" color="secondary" />
+              <IonText>Email</IonText>
+            </div>
+            <div onClick={toggleShowEmail} className={styles.titleContainer}>
+              <IonText>{email.substring(0, 10)}...</IonText>
+              <IonIcon
+                icon={chevronForwardOutline}
+                size="large"
+                color="secondary"
+              />
+            </div>
+            <div
+              id={showEmail ? `` : `${styles.hide}`}
+              className={styles.backgroundBlur}
+            >
+              <IonItem>
+                <IonInput
+                  type="email"
+                  placeholder="Email"
+                  onIonChange={(e) => {
+                    if (!e.detail?.value) {
+                      return;
+                    }
+                    setEmail(e.detail.value);
+                  }}
+                />
+                <IonButton onClick={() => setShowEmail(false)}>Done</IonButton>
+              </IonItem>
+            </div>
+          </div>
+          {/* T - nummer */}
+          <div className={styles.container}>
+            <div className={styles.titleContainer}>
+              <IonIcon icon={callOutline} size="large" color="secondary" />
+              <IonText>T - nummer</IonText>
+            </div>
+            <div onClick={toggleShowNummer} className={styles.titleContainer}>
+              <IonText>{nummer.substring(0, 10)}...</IonText>
+              <IonIcon
+                icon={chevronForwardOutline}
+                size="large"
+                color="secondary"
+              />
+            </div>
+            <div
+              id={showNummer ? `` : `${styles.hide}`}
+              className={styles.backgroundBlur}
+            >
+              <IonItem>
+                <IonInput
+                  type="tel"
+                  placeholder="Adress"
+                  onIonChange={(e) => {
+                    if (!e.detail?.value) {
+                      return;
+                    }
+                    setNummer(e.detail.value);
+                  }}
+                />
+                <IonButton onClick={() => setShowNummer(false)}>Done</IonButton>
+              </IonItem>
+            </div>
+          </div>
         </IonCol>
       </IonGrid>
       <div className={betaal.container}>
--- 1-1-diet/src/components/nieuwProfiel/gegevens/UserSettings.jsx
+++ 1-1-diet/src/components/nieuwProfiel/gegevens/UserSettings.jsx
@@ -13,31 +13,26 @@ export const Settings = [
   {
     icon: transgenderOutline,
     title: "Geslacht",
-    icon2: chevronForwardOutline,
     gegevens: UserProfile[0].gender,
   },
   {
     icon: calendarOutline,
     title: "Geboortedatum",
-    icon2: chevronForwardOutline,
     gegevens: UserProfile[0].birthdate,
   },
   {
     icon: homeOutline,
     title: "Adress",
-    icon2: chevronForwardOutline,
     gegevens: UserProfile[0].adress,
   },
   {
     icon: mailOutline,
     title: "Email",
-    icon2: chevronForwardOutline,
     gegevens: UserProfile[0].email,
   },
   {
     icon: callOutline,
     title: "T - nummer",
-    icon2: chevronForwardOutline,
     gegevens: UserProfile[0].number,
   },
 ];
