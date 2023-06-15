987270f3bbe359c42f64b24a963b28ea90e91bb2
Juicy_Jesse <91599008+JesseElbertsen@users.noreply.github.com>
2023-06-15 11:52:38 +0200

    hoi

--- 1-1-diet/src/components/OrderList/FinishOrder/FinishOrder.tsx
+++ 1-1-diet/src/components/OrderList/FinishOrder/FinishOrder.tsx
@@ -6,10 +6,13 @@ import {
   IonRow,
   IonContent,
   IonButton,
+  IonInput,
 } from "@ionic/react";
 import ptrn from "../../../Images/BACKGROUND 2.svg";
 import ptrn2 from "../images/orderBackground.svg";
 import styles from "./FinishOrder.module.css";
+import idealLogo from "../images/ideal-logo-1024 1.png";
+import masterCardLogo from "../images/Group 51.png";
 
 export default function FinishOrder() {
   return (
@@ -17,7 +20,90 @@ export default function FinishOrder() {
       <IonContent fullscreen>
         <div>
           <img src={ptrn} alt="food ptrn" id={styles.headerptrn} />
-          <div className={styles.content}></div>
+          <div className={styles.content}>
+            <IonGrid>
+              <div>
+                <IonCol>
+                  <div>
+                    <h1>Bestelling afronden</h1>
+                  </div>
+                </IonCol>
+                <hr
+                  style={{
+                    background: "lightgrey",
+                    color: "lightgrey",
+                    borderColor: "lightgrey",
+                    height: "3px",
+                    width: "95%",
+                    opacity: "50%",
+                  }}
+                />
+                <IonCol>
+                  <div className={styles.adresContainer}>
+                    <h2 className={styles.name}>Luka Marjanovic</h2>
+                    <p className={styles.adres}>221b Baker st, Marylebone</p>
+                    <p className={styles.adres}>Londen</p>
+                    <p className={styles.adres}>United Kingdom</p>
+                  </div>
+                </IonCol>
+                <hr
+                  style={{
+                    background: "lightgrey",
+                    color: "lightgrey",
+                    borderColor: "lightgrey",
+                    height: "3px",
+                    width: "95%",
+                    opacity: "50%",
+                  }}
+                />
+                <IonCol>
+                  <div className={styles.containers}>
+                    <IonRow>
+                      <img src={masterCardLogo} alt="mastercard" />
+                    </IonRow>
+                    <IonRow>
+                      <h2 className={styles.pay}>Master Card ending **68</h2>
+                    </IonRow>
+                  </div>
+                </IonCol>
+                <hr
+                  style={{
+                    background: "lightgrey",
+                    color: "lightgrey",
+                    borderColor: "lightgrey",
+                    height: "3px",
+                    width: "95%",
+                    opacity: "50%",
+                  }}
+                />
+                <IonCol>
+                  <div className={styles.containers}>
+                    <img src={idealLogo} alt="Ideal" />
+
+                    <h2 className={styles.pay}>Ideal</h2>
+                  </div>
+                </IonCol>
+                <hr
+                  style={{
+                    background: "lightgrey",
+                    color: "lightgrey",
+                    borderColor: "lightgrey",
+                    height: "3px",
+                    width: "95%",
+                    opacity: "50%",
+                  }}
+                />
+                <IonCol>
+                  <div className={styles.input}>
+                    <IonInput
+                      aria-label="Outline input"
+                      placeholder="Voucher code"
+                    ></IonInput>
+                  </div>
+                </IonCol>
+              </div>
+            </IonGrid>
+          </div>
           <IonGrid>
             <div className={styles.orders}>
               <img src={ptrn2} alt="food ptrn" id={styles.headerptrn2} />
