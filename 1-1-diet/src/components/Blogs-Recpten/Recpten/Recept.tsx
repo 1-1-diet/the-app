42c86347cbd52360aafd4661332b2e48c1152b39
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-06-21 11:46:26 +0200

    save

--- 1-1-diet/src/components/Blogs-Recpten/Recpten/Recept.tsx
+++ 1-1-diet/src/components/Blogs-Recpten/Recpten/Recept.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 import styles from "./Recept.module.css";
+import { IonContent, IonPage } from "@ionic/react";
+import { Recepten } from "../data";
 
 export default function Recept() {
-  return <div></div>;
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        {Recepten?.map((recept) => {
+          return (
+            <div className={styles.mainContainer}>
+              <div className={styles.headerContainer}>
+                <img src="" alt="image" />
+                <div>
+                  {recept.title}
+                  <div>` 1` 121` 12`543 ` ` ` ` </div>
+                </div>
+              </div>
+              <div className={styles.mainContent}></div>
+              <div className={styles.ingridientContainer}></div>
+              <div className={styles.extraContainer}></div>
+            </div>
+          );
+        })}
+      </IonContent>
+    </IonPage>
+  );
 }
