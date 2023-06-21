a2d515307375bafb763d8cca166a0e4ec563896b
Luuk van Dijk <luukvandijk2003@gmail.com>
2023-06-21 13:38:19 +0200

    added recept

--- 1-1-diet/src/components/Blogs-Recpten/Recpten/Recept.tsx
+++ 1-1-diet/src/components/Blogs-Recpten/Recpten/Recept.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./Recept.module.css";
-import { IonContent, IonPage } from "@ionic/react";
+import { IonContent, IonIcon, IonPage, IonText } from "@ionic/react";
 import { Recepten } from "../data";
+
+import { timeOutline, heart, chevronDownOutline } from "ionicons/icons";
 
 export default function Recept() {
   return (
@@ -9,17 +11,65 @@ export default function Recept() {
       <IonContent fullscreen>
         {Recepten?.map((recept) => {
           return (
-            <div className={styles.mainContainer}>
+            <div key={recept.id} className={styles.mainContainer}>
               <div className={styles.headerContainer}>
-                <img src="" alt="image" />
-                <div>
-                  {recept.title}
-                  <div>` 1` 121` 12`543 ` ` ` ` </div>
+                <img src={recept.image} alt="image" />
+                <div className={styles.header__gradient}>
+                  <div className={styles.header__titleContainer}>
+                    <IonText id={styles.header__title}>{recept.title}</IonText>
+                    <div className={styles.header__subContainer}>
+                      <IonText id={styles.header__subTitle}>
+                        <IonIcon icon={heart} />
+                        {recept.yummies} yummies
+                      </IonText>
+                      <IonText id={styles.header__subTitle}>
+                        {recept.bakeTime}
+                        <IonIcon icon={timeOutline} />
+                      </IonText>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className={styles.mainContent}></div>
-              <div className={styles.ingridientContainer}></div>
-              <div className={styles.extraContainer}></div>
+              <div className={styles.mainContent}>
+                <div className={styles.tagsContainer}>
+                  {Recepten[recept.id].tags?.map((tag) => {
+                    return (
+                      <div key={tag.title} className={styles.tag}>
+                        <IonIcon icon={tag.icon} />
+                        {tag.title}
+                      </div>
+                    );
+                  })}
+                </div>
+                <IonText className={styles.discription}>
+                  {recept.discription}
+                </IonText>
+                <div className={styles.ingridientContainer}>
+                  <div className={styles.ingridient__header}>
+                    <h1>Ingridient</h1>
+                    <IonIcon icon={chevronDownOutline} />
+                  </div>
+                  <div className={styles.ingridient__itemContainer}>
+                    {Recepten[recept.id].ingridient?.map((indridient) => {
+                      return (
+                        <div
+                          key={indridient.title}
+                          className={styles.ingridient__item}
+                        >
+                          <div className={styles.ingridient__title}>
+                            <img
+                              src={indridient.image}
+                              alt={indridient.title}
+                            />
+                            <IonText>{indridient.title}</IonText>
+                          </div>
+                          <IonText>{indridient.wheight}</IonText>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
             </div>
           );
         })}
--- 1-1-diet/src/components/Blogs-Recpten/data.jsx
+++ 1-1-diet/src/components/Blogs-Recpten/data.jsx
@@ -1,23 +1,39 @@
 // import images
+import aardbij from "../../Images/aardbij.png";
 
 // import icons
+import {
+  flameOutline,
+  eggOutline,
+  fishOutline,
+  leafOutline,
+} from "ionicons/icons";
 
 export const Recepten = [
   {
-    image: img,
+    id: 0,
+    image: aardbij,
     title: "Yoguhrt met aardbei",
     bakeTime: "5 minuten",
     yummies: "150",
-    tags: [{ icon: icon, title: "80 kcal" }],
+    tags: [
+      { icon: flameOutline, title: "80 kcal" },
+      { icon: eggOutline, title: "80 kcal" },
+      { icon: fishOutline, title: "80 kcal" },
+      { icon: leafOutline, title: "80 kcal" },
+    ],
     discription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales ipsum quis augue laoreet, nec finibus felis dignissim. Aenean dignissim hendrerit aliquam. Aliquam erat volutpat. Nam bibendum purus vel pretium varius. Suspendisse egestas felis eu ipsum fringilla sagittis. Proin consectetur, nisi vitae aliquam pellentesque, tellus sem finibus orci, eu tristique ante libero sit amet elit. Phasellus ornare vel enim non ultrices.",
-    ingridient: [{ image: img, title: "Yoghurt", wheight: "200g" }],
+    ingridient: [
+      { image: aardbij, title: "Yoghurt", wheight: "200g" },
+      { image: aardbij, title: "Aardbiij", wheight: "200g" },
+    ],
   },
 ];
 
 export const Blogs = [
   {
-    image: img,
+    image: aardbij,
     title: "Weer op de weegschaal",
     date: "Sep 4 2022",
     tag: "populair",
